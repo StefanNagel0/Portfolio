b2196d65b72e773ad3096910d95fc6d9e6220600
StefanNagel0 <stefannagel14@hotmail.de>
2025-06-19 20:16:27 +0200

    first commit

---
+++ src/app/whyme/whyme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whyme',
+  standalone: true,
+  imports: [],
+  templateUrl: './whyme.component.html',
+  styleUrl: './whyme.component.scss'
+})
+export class WhymeComponent {
+
+}
