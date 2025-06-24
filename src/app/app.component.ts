ae1daa47389a2f9c11fb082fb19c346920accca5
StefanNagel0 <stefannagel14@hotmail.de>
2025-06-24 21:32:15 +0200

    why me added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AtfComponent } from "./atf/atf.component";
 import { MenubarComponent } from "./menubar/menubar.component";
+import { WhymeComponent } from "./whyme/whyme.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AtfComponent, MenubarComponent],
+  imports: [CommonModule, AtfComponent, MenubarComponent, WhymeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
