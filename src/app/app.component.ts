b4102735ddb617cc28660538407fc8e135aa7b66
StefanNagel0 <stefannagel14@hotmail.de>
2025-06-26 20:43:59 +0200

    mySkills Component added & atf style fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { AtfComponent } from "./atf/atf.component";
 import { MenubarComponent } from "./menubar/menubar.component";
 import { WhymeComponent } from "./whyme/whyme.component";
+import { MyskillsComponent } from "./myskills/myskills.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AtfComponent, MenubarComponent, WhymeComponent],
+  imports: [CommonModule, AtfComponent, MenubarComponent, WhymeComponent, MyskillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/myskills/myskills.component.ts
+++ src/app/myskills/myskills.component.ts
@@ -9,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class MyskillsComponent {
 
+  skillsLogos: string[] = [
+    '/assets/img/skills_logos/angular.png',
+    '/assets/img/skills_logos/typescript.png',
+    '/assets/img/skills_logos/javascript.png',
+    '/assets/img/skills_logos/html.png',
+    '/assets/img/skills_logos/css.png',
+    '/assets/img/skills_logos/firebase.png',
+    '/assets/img/skills_logos/rest_api.png',
+    '/assets/img/skills_logos/git.png',
+    '/assets/img/skills_logos/material_design.png',
+    '/assets/img/skills_logos/scrum.png'    
+  ];
 }
