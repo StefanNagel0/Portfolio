d43fb02b00489ba41ac41758f04752bef8c89620
StefanNagel0 <stefannagel14@hotmail.de>
2025-07-26 11:44:42 +0200

    styling changed

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -9,10 +9,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contactme.component.html',
   styleUrl: './contactme.component.scss'
 })
-export class ContactmeComponent {
+export class ContactmeComponent implements OnInit {
   contactForm: FormGroup;
-  emailClicked: boolean = false;
-
+  emailClicked = false;
+  
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
@@ -22,13 +22,38 @@ export class ContactmeComponent {
     });
   }
 
+  ngOnInit() {
+    this.contactForm.valueChanges.subscribe(() => {
+    });
+
+    this.contactForm.get('name')?.valueChanges.subscribe(() => {
+      this.contactForm.get('name')?.markAsDirty();
+    });
+    
+    this.contactForm.get('email')?.valueChanges.subscribe(() => {
+      this.contactForm.get('email')?.markAsDirty();
+    });
+    
+    this.contactForm.get('message')?.valueChanges.subscribe(() => {
+      this.contactForm.get('message')?.markAsDirty();
+    });
+  }
+
   onSubmit() {
     if (this.contactForm.valid) {
       console.log('Form Data:', this.contactForm.value);
-      // Hier könnten Sie eine API-Anfrage senden
+      
       this.contactForm.reset();
+      
+      Object.keys(this.contactForm.controls).forEach(key => {
+        const control = this.contactForm.get(key);
+        control?.markAsUntouched();
+        control?.markAsPristine();
+      });
+      
+      this.contactForm.get('privacy')?.setValue(false);
     } else {
-      this.contactForm.markAllAsTouched(); // Zeigt Fehler an
+      this.contactForm.markAllAsTouched();
     }
   }
 }
